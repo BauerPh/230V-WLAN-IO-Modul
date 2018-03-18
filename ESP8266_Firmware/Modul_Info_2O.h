1e8a8a08a2861fde25fd88a65b7ea897463b4fe8
Philipp Bauer <36240461+BauerPh@users.noreply.github.com>
2018-03-19 00:42:31 +0100

    fix SSID check

--- ESP8266_Firmware/Modul_Info_2O.h
+++ ESP8266_Firmware/Modul_Info_2O.h
@@ -1,5 +1,5 @@
 #define MODEL_NAME "ESP_230V_IO_UP_2O"
-#define VERSION "1.3.0"
+#define VERSION "1.3.1"
 //************************ IOs ***************************
 #define INPUT1_PIN 12
 #define INPUT2_PIN 13
